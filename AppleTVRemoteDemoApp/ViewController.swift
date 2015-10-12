8ecd0f483e60148e479f98932438dde93a6f7239
Luciano Marisi <luciano@techbrewers.com>
2015-10-12 22:41:35 +0100

    Initial Commit

---
+++ AppleTVRemoteDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTVRemoteDemoApp
+//
+//  Created by Luciano Marisi on 12/10/2015.
+//  Copyright © 2015 TechBrewers LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
